a62854966c75879ebc8434f1e530c4c496a61de9
Yoohogyun <yoohoogun114@naver.com>
2018-03-13 11:24:06 +0900

    Disabled Auth

--- src/app.ts
+++ src/app.ts
@@ -31,14 +31,27 @@ import * as passportConfig from "./config/passport";
 const app = express();
 
 // Connect to MongoDB
+/*
 const mongoUrl = process.env.MONGOLAB_URI;
 (<any>mongoose).Promise = bluebird;
 mongoose.connect(mongoUrl, {useMongoClient: true}).then(
-  () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+  () => {
+    // ready to use. The `mongoose.connect()` promise resolves to undefined.
+  },
 ).catch(err => {
   console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
   // process.exit();
 });
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET,
+  store: new MongoStore({
+    url: mongoUrl,
+    autoReconnect: true
+  })
+}));
+*/
 
 // Express configuration
 app.set("port", process.env.PORT || 8081);
@@ -49,17 +62,8 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: process.env.SESSION_SECRET,
-  store: new MongoStore({
-    url: mongoUrl,
-    autoReconnect: true
-  })
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use(flash());
 app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
@@ -67,6 +71,7 @@ app.use((req, res, next) => {
   res.locals.user = req.user;
   next();
 });
+/*
 app.use((req, res, next) => {
   // After successful login, redirect back to the intended page
   if (!req.user &&
@@ -81,6 +86,7 @@ app.use((req, res, next) => {
   }
   next();
 });
+*/
 
 app.use(
   // express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
@@ -122,15 +128,15 @@ app.get("/", function(req, res) {
 /**
  * API examples routes.
  */
-app.get("/api", apiController.getApi);
-app.get("/api/facebook", passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFacebook);
+// app.get("/api", apiController.getApi);
+// app.get("/api/facebook", passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFacebook);
 
 /**
  * OAuth authentication routes. (Sign in)
  */
-app.get("/auth/facebook", passport.authenticate("facebook", { scope: ["email", "public_profile"] }));
-app.get("/auth/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/login" }), (req, res) => {
-  res.redirect(req.session.returnTo || "/");
-});
+// app.get("/auth/facebook", passport.authenticate("facebook", { scope: ["email", "public_profile"] }));
+// app.get("/auth/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/login" }), (req, res) => {
+//   res.redirect(req.session.returnTo || "/");
+// });
 
 export default app;
